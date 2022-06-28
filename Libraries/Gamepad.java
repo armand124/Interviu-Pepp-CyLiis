661335c5b534fa541c677c0b06b837ee2d352d27
Armand <52544834+armand124@users.noreply.github.com>
2022-06-28 21:28:33 +0300

    Add files via upload

---
+++ Libraries/Gamepad.java
@@ -0,0 +1,4 @@
+public class Gamepad
+{
+    enum{}
+}
---
+++ Libraries/Servo.java
@@ -0,0 +1,18 @@
+public class Servo 
+{
+    public Servo()
+    {
+
+    }
+    public void setPosition(int gradeHexa)
+    {
+        // seteaza pozitia servoului intr-un interval de [0-360]
+
+    }
+    public int getPosition()
+    {
+        //returneaza valuarea la care se afla servo-ul
+        return -1;
+    }
+
+}
