9b3a4e1742761690dbb31806a5d31f938f360b92
Armand <52544834+armand124@users.noreply.github.com>
2022-06-28 21:32:25 +0300

    Main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    boolean robotIsActive() {
+        // Intoarce True daca robotul este pornit
+        // Intoarce False daca butonul de oprire a fost apasat
+    }
+    
+}
